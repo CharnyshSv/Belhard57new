9a8211e0b68457bf74fdf025c087474b8ee8f6e8
CharnyshSv <sv.charnysh@gmail.com>
2022-08-10 20:17:56 +0300

    resolve

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from crud import CRUDCategory
 
-category = CRUDCategory.get(category_id=1)
-print(category)
-category.name = 'Еда'
-CRUDCategory.update(category=category)
-print(CRUDCategory.get(category_id=1))
+#category = CRUDCategory.get(category_id=1)
+#print(category)
+#category.name = 'Еда'
+#CRUDCategory.update(category=category)
+#print(CRUDCategory.get(category_id=1))
 
 
 #CRUDCategory.add(name="Стеки", parent_id=1)
@@ -13,4 +13,17 @@ print(CRUDCategory.get(category_id=1))
 #    print(category.name)
 #    print(category.__dict__)
 
+#async def main():
+#    res = await CRUDArticle.get_all()
+#    print(res)
+#asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+#asyncio.run(main())
+import asyncio
+
+
+async def main():
+    res = await CRUDCategory.add
+    print(res)
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
 
